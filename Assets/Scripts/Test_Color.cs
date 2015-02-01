66ad088773846724fd683795e86009997796b970
Punllarena <cedyclark@yahoo.com>
2015-02-01 13:08:24 +0800

    Edited Test_color.cs
    -Random fixed. can now select 6.
    -Each cube will get a color every time you press spacebar
    -Cubes on scene were numbered for debugging purposes
    -Added Directional light to make the cubes' color more vivid.

--- Assets/Scripts/Test_Color.cs
+++ Assets/Scripts/Test_Color.cs
@@ -4,46 +4,45 @@ using System.Collections;
 public class Test_Color : MonoBehaviour {
 
 	public int randomColor;
-	public int red = 1;
+	/*public int red = 1;
 	public int blue = 2;
 	public int green = 3;
 	public int yellow = 4;
-	public int pink = 5;
-	public int white =6;
+	public int magenta = 5;
+	public int black =6;*/
 	public float min = 1f;
-	public float max = 6f;
-
+	public float max = 7f;
+	//public Color pinkColor = new Color(1,0,1);
 
 	void Update () {
 
+		randomColor = (int) Random.Range (min,max);
+
 		if (Input.GetKeyDown(KeyCode.Space))
 			colorTest ();
-
-		randomColor = (int) Random.Range (min,max);
 
 	}
 
 	void colorTest()
 	{
-		if (randomColor == 1)
-			Debug.Log("Red");
-		else if (randomColor == 2)
-			Debug.Log("Blue");
-		else if (randomColor == 3)
-			Debug.Log("Green");
-		else if (randomColor == 4)
-			Debug.Log("Yellow");
-		else if (randomColor == 5)
-			Debug.Log("Pink");
-		else if (randomColor == 6)
-			Debug.Log("White");
-		/* Debug.Log("Red"); break;
-		Debug.Log("Blue"); break;
-		case 3: Debug.Log("Green"); break;
-		case 4: Debug.Log("Yellow"); break;
-		case 5: Debug.Log("Pink"); break;
-		case 6:	Debug.Log("White"); break;
-		}*/
-	
+		if (randomColor == 1) {
+						Debug.Log ("Red");
+						gameObject.renderer.material.color = Color.red;			
+				} else if (randomColor == 2) {
+						Debug.Log ("Blue");
+						gameObject.renderer.material.color = Color.blue;
+				} else if (randomColor == 3) {
+						Debug.Log ("Green");
+						gameObject.renderer.material.color = Color.green;
+				} else if (randomColor == 4) {
+						Debug.Log ("Yellow");
+						gameObject.renderer.material.color = Color.yellow;
+				} else if (randomColor == 5){
+						Debug.Log("Magenta");
+						gameObject.renderer.material.color = Color.magenta;
+				} else if (randomColor == 6){
+						Debug.Log("White");
+						gameObject.renderer.material.color = Color.white;
+				}
 	}
 }
