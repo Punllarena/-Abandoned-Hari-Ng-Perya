8f2e110529bfe6fb55d5cea049306014440c2467
MarkRheine <mark.azul@bicol-u.edu.ph>
2015-02-14 10:54:05 +0800

    menu_script_not_doing_anything
    cedy- patingin kung ano ung kelangan..

---
+++ Assets/Main_Menu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_Menu : MonoBehaviour 
+{
+	
+	public bool isQuit = false;
+	
+	void OnMouseEnter()
+	{
+		gameObject.GetComponent<TextMesh>().color = Color.black;    
+	}
+	
+	void OnMouseExit()
+	{
+		gameObject.GetComponent<TextMesh>().color = Color.white;
+	}
+	
+	void OnMouseDown()
+	{
+			Application.Quit();    
+		
+	}
+}
---
+++ Assets/Scenes/Main Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuObject : MonoBehaviour 
+{
+	
+	public bool isQuit = false;
+	
+	void OnMouseEnter()
+	{
+		renderer.material.color = Color.blue;    
+	}
+	
+	void OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+	}
+	
+	void OnMouseDown()
+	{
+		if(isQuit)
+		{
+			Application.Quit();    
+		}
+		else
+		{
+			Application.LoadLevel(1); //Make sure that level "1" is located in your build settings. You can also change the 1 with a name of your scene.
+			
+		}
+	}
+}
